b32ad45d3e7cb0e7d97a80f176c898bc7d9b8d57
Yaroslav.S <sysoev.yar@yandex.ru>
2022-02-28 21:06:44 +0500

    Introl Funcion

--- Example_1/Program.cs
+++ Example_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Нахождение максимума из 9 переменных
+
+int Max(int arg1, int arg2, int arg3)       // функция
+{
+    int rezult = arg1;
+    if (arg2 > rezult) rezult = arg2;
+    if (arg3 > rezult) rezult = arg3;
+    return rezult;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 113;
+int b3 = 23;
+int c3 = 33;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
